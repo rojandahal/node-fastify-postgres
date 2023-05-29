6f9f4193d72bb0595e7215186d7658695f84f666
rojandahal <maxpanye60@gmail.com>
2023-05-29 15:22:16 +0545

    Local Authentication Performed

--- controller/handler/userHandler.js
+++ controller/handler/userHandler.js
@@ -43,23 +43,29 @@ const getUsers = async (req, reply) => {
 const updateUsers = async (req, reply) => {
   const { id } = req.params;
   const fastify = req.server;
-  const user = await fastify.user.findOne({ where: { id: id } });
-  user === null ? reply.status(404).send(`User with id ${id} not found`) : null;
+  try {
+    const user = await fastify.user.findByPk(id);
+    user === null
+      ? reply.status(404).send(`User with id ${id} not found`)
+      : null;
 
-  const tokenDecoded = await req.server.getUserFromToken(req, reply);
+    const tokenDecoded = await req.server.getUserFromToken(req, reply);
 
-  if (user.username === tokenDecoded.dataValues.username) {
-    const updatedUser = { username: req.body.username };
-    await user.update(updatedUser);
-    const token = req.server.jwt.sign(user);
-    // Set the token as a cookie
-    reply.setCookie('token', token, {
-      expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // Expires in 24 hours
-    });
+    if (user.username === tokenDecoded.dataValues.username) {
+      const updatedUser = { username: req.body.username };
+      await user.update(updatedUser);
+      const token = req.server.jwt.sign(user);
+      // Set the token as a cookie
+      reply.setCookie('token', token, {
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // Expires in 24 hours
+      });
 
-    reply.code(200).send({ 'User Updated': user });
-  } else {
+      reply.code(200).send({ 'User Updated': user });
+    }
     reply.code(401).send({ Unauthorized: 'Unauthorized' });
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.errors[0].message);
   }
 };
 
@@ -81,7 +87,7 @@ const deleteUser = async (req, reply) => {
 const getUser = async (req, reply) => {
   const { id } = req.params;
   const fastify = req.server;
-  const user = await fastify.user.findOne({ where: { id: id } });
+  const user = await fastify.user.findByPk(id);
   user === null ? reply.status(404).send(`User with id ${id} not found`) : null;
   const tokenDecoded = await req.server.getUserFromToken(req, reply);
 
