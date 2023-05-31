e957f6bfb4dc1d34137157063fc5ea8b6e141f3a
rojandahal <maxpanye60@gmail.com>
2023-05-31 10:34:41 +0545

    fix:session expiry fixed

--- plugins/session.js
+++ plugins/session.js
@@ -14,8 +14,8 @@ const sessionConnect = async (fastify, opts) => {
     cookie: {
       secure: false, // Set to true if using HTTPS
       // maxAge: 24 * 60 * 60 * 1000, // Session expiration time (in milliseconds)
-      maxAge: 60000,
-      expires: new Date(Date.now() + 60000),
+      maxAge: 6000,
+      expires: new Date(Date.now() + 6000),
     },
     saveUninitialized: false,
     sameSite: true,
@@ -24,47 +24,6 @@ const sessionConnect = async (fastify, opts) => {
       tableName: 'sessions', // Name of the session table in the database
       createTableIfMissing: false, // Disable table creation if missing
     }),
-  });
-
-  // fastify.decorate('updateTokenExpiry', async function (req, reply, done) {
-  //   if (req.session && req.session.cookie && req.session.cookie.expires) {
-  // 		const sessionRecord = new Date(req.session.cookie.expires);
-  // 		const sessionExpirationTime = sessionRecord.getTime();
-  // 		const currentTime = Date.now();
-  // 		console.log(sessionExpirationTime);
-  // 		console.log(currentTime)
-
-  //     console.log(currentTime > sessionExpirationTime);
-  //     if (currentTime >= sessionExpirationTime) {
-  //       // Session has expired
-  //       // Handle the expired session scenario
-  // 			console.log('Session Expired, Unauthorized')
-  //       reply.code(401).send({ message: 'Session Expired, Unauthorized' });
-  //     } else {
-  //       // Session is still active
-  //       // Proceed with the protected route logic
-  //       // ...
-  //       // reply.code(200).send({ message: 'Session is Still Active' });
-  //     }
-  //   } else {
-  //     // Session information not available or missing expiration time
-  //     // Handle the scenario accordingly
-  //     res.redirect('/login');
-  //   }
-  // });
-
-  fastify.decorate('authenticate', async function (req, reply, done) {
-    const token = req.session.userId;
-    try {
-      if (!token) {
-        reply.code(401).send({ error: 'Unauthorized' });
-        return done();
-      }
-    } catch (err) {
-      console.error(err);
-      throw new Error(err);
-    }
-    done();
   });
 };
 // const fastifyPlugin = require('fastify-plugin');
